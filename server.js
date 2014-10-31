7dc4f83119215e9f1100dff5e53fe697b90cc985
= <=>
2014-10-30 22:10:54 -0200

    Primeiro commit

--- server.js
+++ server.js
@@ -1,86 +1,52 @@
 //Setup web server and socket
-var twitter = require('twitter'),
+var twitter = require('ntwitter'),
     express = require('express'),
     app = express(),
     http = require('http'),
     server = http.createServer(app),
     io = require('socket.io').listen(server);
 
-//Setup twitter stream api
 var twit = new twitter({
-  consumer_key: '<ENTER>',
-  consumer_secret: '<ENTER>',
-  access_token_key: '<ENTER>',
-  access_token_secret: '<ENTER>'
-}),
-stream = null;
-
-//Use the default port (for beanstalk) or default to 8081 locally
-server.listen(process.env.PORT || 8081);
-
-//Setup rotuing for app
-app.use(express.static(__dirname + '/public'));
-
-//Create web sockets connection.
-io.sockets.on('connection', function (socket) {
-
-  socket.on("start tweets", function() {
-
-    if(stream === null) {
-      //Connect to twitter stream passing in filter for entire world.
-      twit.stream('statuses/filter', {'locations':'-180,-90,180,90'}, function(stream) {
-          stream.on('data', function(data) {
-              // Does the JSON result have coordinates
-              if (data.coordinates){
-                if (data.coordinates !== null){
-                  //If so then build up some nice json and send out to web sockets
-                  var outputPoint = {"lat": data.coordinates.coordinates[0],"lng": data.coordinates.coordinates[1]};
-
-                  socket.broadcast.emit("twitter-stream", outputPoint);
-
-                  //Send out to web sockets channel.
-                  socket.emit('twitter-stream', outputPoint);
-                }
-                else if(data.place){
-                  if(data.place.bounding_box === 'Polygon'){
-                    // Calculate the center of the bounding box for the tweet
-                    var coord, _i, _len;
-                    var centerLat = 0;
-                    var centerLng = 0;
-
-                    for (_i = 0, _len = coords.length; _i < _len; _i++) {
-                      coord = coords[_i];
-                      centerLat += coord[0];
-                      centerLng += coord[1];
-                    }
-                    centerLat = centerLat / coords.length;
-                    centerLng = centerLng / coords.length;
-
-                    // Build json object and broadcast it
-                    var outputPoint = {"lat": centerLat,"lng": centerLng};
-                    socket.broadcast.emit("twitter-stream", outputPoint);
-
-                  }
-                }
-              }
-              stream.on('limit', function(limitMessage) {
-                return console.log(limitMessage);
-              });
-
-              stream.on('warning', function(warning) {
-                return console.log(warning);
-              });
-
-              stream.on('disconnect', function(disconnectMessage) {
-                return console.log(disconnectMessage);
-              });
-          });
-      });
-    }
-  });
-
-    // Emits signal to the client telling them that the
-    // they are connected and can start receiving Tweets
-    socket.emit("connected");
+  consumer_key: 'pqEd7Khb0PFqCUVqZ8ASWz9IJ',
+  consumer_secret: 'shuE3hq8zVnjEErxS4YOGsDWthBiBaGjKI2mto6b2GPMlSTMgA',
+  access_token_key: '67066867-diE9MKlzruRVIMhdt6lAJUR5pQNx7FlKMVuFA13nv',
+  access_token_secret: 'MHtPmMaDu4iBJdYKVMxkAoJ7MExkCeNjIK6PHIpIAi6Cr'
 });
+
+twit.verifyCredentials(function (err, data) {
+    if(err !== null){
+        console.log(err);
+    }else{
+      console.log("Login Realizado!");  
+    }
+    
+});
+
+
+var connectCounter = 0;
+
+io.on('connect', function() { 
+  connectCounter++; 
+  console.log(connectCounter);
+});
+io.on('disconnect', function() { 
+  connectCounter--; 
+  console.log(connectCounter);
+});
+
+twit.stream('statuses/filter', { track: ['neymar', 'messi', 'cr7', 'barcelona', 'real madrid'] }, function(stream) {
+  stream.on('data', function (data) {
+     io.sockets.emit('tweet', {
+      user: data.user.screen_name,
+      text: data.text
+    });
+  });
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(2000);
+
 
